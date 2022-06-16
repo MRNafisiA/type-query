b81bd9f7769d3cfec26d093c36ac9385fb2bfacf
MRNafisiA <mnafisiasl@gmail.com>
2022-06-16 14:50:53 +0430

    import some types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import U from './U';
 import PostgresErrors from './error';
+import type {Pool} from './types/pool';
 import {createContext} from './context';
+import type {Context} from './types/context';
 import type {ClientBase, PoolClient} from 'pg';
 import type {SimpleModel, Model} from './types/model';
 import {createPool, addHook, removeHook} from './pool';
+import type {JSON, JsonObject, JsonArray, BaseJsonValue} from './types/json';
 import {
     createEntity,
     resolveResult,
     resolveReturning,
     resolveExpression
 } from './entity';
+import type {
+    OrderDirection,
+    PostgresType,
+    PostgresTypeMapper,
+    ColumnTypeByColumns,
+    ColumnTypeByTable
+} from './types/postgres';
 import {
     createTables,
     dropTables,
@@ -20,12 +30,60 @@ import {
     dropTableSQL,
     getSequenceName
 } from './schema';
+import type {
+    JoinType,
+    Param,
+    QueryData,
+    TableData,
+    JoinData,
+    Mode,
+    CustomColumn,
+    ExpressionTypes,
+    Expression,
+    ValueExpression,
+    QueryExpression,
+    InsertValue,
+    UpdateSets,
+    Query,
+    QueryResult,
+    QueryResultRow,
+    PartialQuery
+} from './types/entity';
 
+export type {Pool};
+export type {Context};
+export type {ClientBase, PoolClient};
+export type {SimpleModel, Model};
+export type {JSON, JsonObject, JsonArray, BaseJsonValue};
+export type {
+    OrderDirection,
+    PostgresType,
+    PostgresTypeMapper,
+    ColumnTypeByColumns,
+    ColumnTypeByTable
+};
+export type {
+    JoinType,
+    Param,
+    QueryData,
+    TableData,
+    JoinData,
+    Mode,
+    CustomColumn,
+    ExpressionTypes,
+    Expression,
+    ValueExpression,
+    QueryExpression,
+    InsertValue,
+    UpdateSets,
+    Query,
+    QueryResult,
+    QueryResultRow,
+    PartialQuery
+};
 export {U};
 export {PostgresErrors};
 export {createContext};
-export {ClientBase, PoolClient};
-export {SimpleModel, Model};
 export {createPool, addHook, removeHook};
 export {
     createEntity,
